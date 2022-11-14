335fe26d45707d82602ddbad52b05c8a9b69d56c
Nael1820 <94629276+Nael1820@users.noreply.github.com>
2022-11-14 18:32:48 +0800

    Add files via upload

---
+++ assets/preload/scripts/death.lua
@@ -0,0 +1,6 @@
+function onCreate()
+		setPropertyFromClass('GameOverSubstate', 'characterName', 'DEATH LOSER');
+		setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'nido_ded');
+		setPropertyFromClass('GameOverSubstate', 'loopSoundName', '');
+	    setPropertyFromClass('GameOverSubstate', 'endSoundName', '');
+end
